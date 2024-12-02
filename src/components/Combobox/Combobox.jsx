b0e3cd0b1b781af7cb0d62873ac2bbed97103ee4
Sahab Ranjbar <amirisahab@gmail.com>
2024-12-02 21:14:49 +0330

    [CREATE] intialize combobox

---
+++ src/components/Combobox/Combobox.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Combobox = () => {
+  return (
+    <div>
+        <select name="" id="">
+            <option value="hello">hello</option>
+            <option value="hello">hello</option>
+            <option value="hello">hello</option>
+            <option value="hello">hello</option>
+        </select>
+    </div>
+  )
+}
+
+export default Combobox
