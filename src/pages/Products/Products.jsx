ffd29556c52cea3ae2012fd56883ddfeb0c0be7f
Sahab Ranjbar <amirisahab@gmail.com>
2024-12-04 00:17:18 +0330

    change product data by add context

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import useGetData from '../../hooks/useGetData/useGetData'
 import ProductCard from './components/ProductCard'
 import './styles/Products.css'
 import Combobox from '../../components/Combobox/Combobox'
+import { ProductsContext } from '../Home/context/ProductsContext'
+import Loading from '../../components/Loading/Loading'
 
 const Products = () => {
 
-    const { data: productsData } = useGetData({
-        url: "https://kaaryar-ecom.liara.run/v1/products?page=1&limit=10"
-    })
+    const { productsData, productsLoading } = useContext(ProductsContext)
     return (
         <>
             <div className='product-header'>
-                <div className='flex justify-between p-2'>
+                {/* <div className='flex justify-between p-2'>
                     <div className='flex gap '>
                         <div className='flex items-center gap'>
                             SORT BY: <Combobox />
@@ -25,27 +25,28 @@ const Products = () => {
                     <div>
                         view
                     </div>
-                </div>
-
-                <div className='products-container'>
-                    {productsData?.products?.map((product, index) => {
-                        console.log(product);
-
-                        return (
-                            <ProductCard
-                                alt={product?.name}
-                                category={product?.category}
-                                description={product?.description}
-                                id={product?._id}
-                                name={product?.name}
-                                price={product?.price}
-                                src={product?.images}
-                                key={product?._id}
-                                rate={index}
-                            />
-                        )
-                    })}
-                </div>
+                </div> */}
+                {productsLoading ?
+                    <Loading />
+                    :
+                    <div className='products-container'>
+                        {productsData?.products?.map((product, index) => {
+                            return (
+                                <ProductCard
+                                    alt={product?.name}
+                                    category={product?.category}
+                                    description={product?.description}
+                                    id={product?._id}
+                                    name={product?.name}
+                                    price={product?.price}
+                                    src={product?.images}
+                                    key={product?._id}
+                                    rate={index}
+                                />
+                            )
+                        })}
+                    </div>
+                }
             </div>
 
         </>
