3f71d6f4688df389a372b39f2aca1dfd5b0d0a3d
Sahab Ranjbar <amirisahab@gmail.com>
2024-12-04 00:12:56 +0330

    modify *Filter* and create components

--- src/pages/Filter/Filter.jsx
+++ src/pages/Filter/Filter.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import './styles/Filter.css'
+import Categories from './components/Categories'
+import Price from './components/Price'
+import Brand from './components/Brand'
+import TopSelling from './components/TopSelling'
 
 const Filter = () => {
     return (
-        <div>Filter</div>
+        <div className='filter'>
+            <div className='categories'>
+                <Categories />
+            </div>
+
+            <div className='price'>
+                <Price />
+            </div>
+
+            <div className='Brand'>
+                <Brand />
+            </div>
+
+            <div className='topSelling'>
+                <TopSelling />
+            </div>
+        </div>
     )
 }
 
