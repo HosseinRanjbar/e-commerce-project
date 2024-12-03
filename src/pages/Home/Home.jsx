ba0de5bd783b549c626a68c503f6aa22e3ba5855
Sahab Ranjbar <amirisahab@gmail.com>
2024-12-04 00:15:19 +0330

    create products context for fetch data by changing categories

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,26 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import MainContent from '../../components/MainContent/MainContent'
 import Footer from '../../components/Footer/Footer'
+import { ProductsContext } from './context/ProductsContext'
+import useGetData from '../../hooks/useGetData/useGetData'
 
 const Home = () => {
+    const { data: productsData, loading, fetchData } = useGetData({
+        url: "https://kaaryar-ecom.liara.run/v1/products?page=1&limit=10"
+    })
+
     return (
         <>
             <Header />
-            <MainContent />
+            <ProductsContext.Provider
+                value={{
+                    productsfetchData: fetchData,
+                    productsData,
+                    productsLoading: loading
+                }}
+            >
+                <MainContent />
+            </ProductsContext.Provider>
             <Footer />
         </>
     )
