2bc31c7406fee6724767b44939b02764b8be41be
Sahab Ranjbar <amirisahab@gmail.com>
2024-12-06 01:06:13 +0330

    [update] *Button* component and set defaultButton prop to get default button and ...res for it

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -6,12 +6,17 @@ const Button = ({
     children,
     color = "primary",
     borderRadius,
-    className
+    className,
+    defaultButton,
+    cursor = "pointer",
+    style,
+    ...res
 }) => {
     return (
         <button
-            className={classNames(className, "btn", { "btn-primary": color === "primary", "btn-red": color === "red", "btn-warning": color === "warning" })}
-            style={{ borderRadius }}
+            className={!defaultButton ? classNames(className, "btn", { "btn-primary": color === "primary", "btn-red": color === "red", "btn-warning": color === "warning" }) : className}
+            style={{ borderRadius, cursor, ...style }}
+            {...res}
         >
             {children}
         </button>
