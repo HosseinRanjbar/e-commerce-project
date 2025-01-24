45ec415d3e038c2359b0dfd28fd2e6531bbe9b03
Sahab Ranjbar <amirisahab@gmail.com>
2025-01-24 18:37:49 +0330

    modify *useGetData* hook

--- src/hooks/useGetData/useGetData.jsx
+++ src/hooks/useGetData/useGetData.jsx
@@ -19,45 +19,48 @@ const useGetData = ({
         error: null
     })
 
+
+
     const fetchData = useCallback(
         async (inputUrl, inputParams = {}, inputPage, inputPageSize) => {
-            return new Promise(async (resoleve, reject) => {
-                try {
-                    setState({ data: null, error: null, loading: true });
+            return new Promise(async (resolve, reject) => {
 
-                    //TODO: check the last character is "?" or not
-                    const response = await fetch(`${inputUrl || url}?${getParams(params) + getParams(inputParams)}${inputPage ? `&page=${inputPage}` : page ? `&page=${page}` : ""}${inputPageSize ? `&limit=${inputPageSize}` : pageSize ? `&limit=${pageSize}` : ""}`, {
-                        method,
-                        body: body ? JSON.stringify(body) : null,
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                    })
+                setState({ data: null, error: null, loading: true });
 
+                //TODO: check the last character is "?" or not
+                await fetch(`${inputUrl || url}?${getParams(params) + getParams(inputParams)}${inputPage ? `&page=${inputPage}` : page ? `&page=${page}` : ""}${inputPageSize ? `&limit=${inputPageSize}` : pageSize ? `&limit=${pageSize}` : ""}`, {
+                    method,
+                    body: body ? JSON.stringify(body) : null,
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }).then((response) => {
                     if (!response.ok) {
                         throw new Error(`HTTP error! status: ${response.status}`);
                     }
 
-                    const data = await response.json();
+                    return response.json()
 
+                }).then((data) => {
                     setState({ data, error: null, loading: false });
                     onSuccess && onSuccess(data)
-                    resoleve(data)
-                } catch (err) {
+                    resolve(data)
+                }).catch((err) => {
                     setState({ data: null, error: err.message, loading: false });
                     onFailur && onFailur(err)
                     reject(err)
-                }
+                })
+
             })
 
 
-        }, [url, method, body, onSuccess, onFailur])
+        }, [url, method, body])
 
     useEffect(() => {
 
         fetchFirst && fetchData();
 
-    }, [fetchFirst])
+    }, [])
 
     return ({ ...state, fetchData });
 }
